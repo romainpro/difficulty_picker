56c735d84f323c83d898b28847cf57c36e7e0b78
romain.casagrande <romain.casagrande.pro@gamil.com>
2023-09-04 11:13:55 +0200

    updateDifficulty

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty.jsx";
 import { MenuListe } from "./components/MenuList/MenuList.jsx";
 
 export function App(){
+    const [currentDifficulty,setCurrentDifficulty] = useState("");
+
+    function updateDifficulty(difficulty){
+        setCurrentDifficulty(difficulty);
+    }
     return<div>
-        {/*<DisplayDifficulty difficulty="Low" />*/}
-       <MenuListe/>
+        {<DisplayDifficulty difficulty={currentDifficulty} />}
+       <MenuListe difficulty={currentDifficulty}/>
     </div>;
 }
