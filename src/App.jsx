6be7477e7cd6b87439d4d3ba27c7da76e79e49d5
romain <romain.casagrande.pr@gmail.com>
2023-09-04 16:48:41 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty.jsx";
 import { MenuListe } from "./components/MenuList/MenuList.jsx";
+import s from "./style.module.css";
 
 export function App(){
     const [currentDifficulty,setCurrentDifficulty] = useState("");
@@ -9,7 +10,10 @@ export function App(){
         setCurrentDifficulty(difficulty);
     }
     return<div>
+        <h1 className={s.title}>Select your React difficulty</h1>
+        <div className={s.workspace}>
        <MenuListe onItemClick={updateDifficulty} difficulty={currentDifficulty}/>
-        {<DisplayDifficulty difficulty={currentDifficulty} />}
+        <DisplayDifficulty difficulty={currentDifficulty} />
+        </div>
     </div>;
 }
